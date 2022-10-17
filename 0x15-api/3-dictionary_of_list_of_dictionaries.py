3a343910a13f63cca4e33a56332081151649ef58
DominicSamoes <dominicsamo@outlook.com>
2022-10-17 23:03:16 +0200

    0x15 Tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Exports data in the JSON format"""
 
-if __name__ == "__main__":
+import json
+import requests
+import sys
 
-    import json
-    import requests
-    import sys
+if __name__ == "__main__":
 
     users = requests.get("https://jsonplaceholder.typicode.com/users")
     users = users.json()
